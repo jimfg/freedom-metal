1ad08444d27c7c3774f35a5f89c8262f82301e6a
jimfg <jinb001@sjtu.edu.cn>
2023-07-09 21:05:35 +0800

    modify _read in gloss, to pass sifive/uart-test

--- gloss/sys_read.c
+++ gloss/sys_read.c
@@ -1,7 +1,23 @@
 #include <errno.h>
 #include <sys/types.h>
+#include <metal/tty.h>
+#include <unistd.h>
 
 ssize_t _read(int file, void *ptr, size_t len) {
-    errno = ENOSYS;
-    return -1;
+    if (file != STDIN_FILENO){
+        errno = ENOSYS;
+        return -1;
+    }
+    char *bptr = (char *)ptr;
+    int c;
+    size_t i;
+    for (i = 0; i < len; ++i){
+        metal_tty_getc(&c);
+	if (c == '\r')
+	    c = '\n';
+	*(bptr + i) = (char)c;
+	if (c == '\n')
+	    return i + 1;
+    }
+    return i;
 }
